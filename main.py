cbf92e7a738efc62e9bb84108772ac9e057d1757
Francisco José <chicoze247@gmail.com>
2024-09-17 19:44:15 -0300

    up

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import pandas as pd
 
 # Carregar o arquivo TXT
 txt_file = 'relatorio.txt'
-
-# Carregar dados TXT em DataFrame
 data_txt = pd.read_csv(txt_file, sep='|')
 
 # Carregar o arquivo Excel
 excel_file = 'VENDAS.xlsx'
 data_excel = pd.read_excel(excel_file)
 
-# Supondo que o Excel também tenha uma coluna com o número do chassi para fazer o merge
-# O nome da coluna deve ser ajustado conforme o nome real no arquivo Excel.
-# Vamos supor que a coluna de chassi no Excel seja 'CHASSI_VENDIDO'
-df_merged = pd.merge(data_txt, data_excel, on='Chassi', how='inner')
+# Exibir colunas dos DataFrames para verificar os nomes
+print("Colunas no arquivo TXT:", data_txt.columns)
+print("Colunas no arquivo Excel:", data_excel.columns)
+
+# Verifique o nome correto da coluna de chassi em ambos os DataFrames e ajuste aqui
+# Exemplo: Se a coluna no Excel for chamada de 'Chassi', mas no TXT for 'CHASSI_VENDIDO'
+df_merged = pd.merge(data_txt, data_excel, left_on='CHASSI_VENDIDO', right_on='Chassi', how='left')
 
 # Salvar a nova planilha com os dados combinados
-df_merged.to_excel('/mnt/data/planilha_combinada.xlsx', index=False)
+df_merged.to_excel('planilha_combinada.xlsx', index=False)
 
 print("Planilha combinada criada com sucesso!")
