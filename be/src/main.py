38f8e94e33748efd3f44dc78144a4d02345e72fc
Francisco José <chicoze247@gmail.com>
2024-09-23 11:31:32 -0300

    2

--- be/src/main.py
+++ be/src/main.py
@@ -1,38 +1,5 @@
 from db.connector import connect
 
 def main():
-    # Estabelece a conexão com o banco de dados
-    conexao = connect()
-    
-    if conexao:
-        try:
-            # Cria um cursor para executar queries
-            cursor = conexao.cursor()
-            
-            # Exemplo de query
-            cursor.execute("SELECT VERSION()")
-            
-            # Recupera o resultado
-            resultado = cursor.fetchone()
-            
-            # Imprime o resultado
-            print(f"Versão do MySQL: {resultado[0]}")
-            
-        except Exception as e:
-            print(f"Erro ao executar query: {e}")
-        
-        finally:
-            # Fecha o cursor e a conexão
-            if 'cursor' in locals():
-                cursor.close()
-            conexao.close()
-            print("Conexão fechada.")
-    else:
-        print("Não foi possível conectar ao banco de dados.")
-        return 0
-    
-    
-
-
-if __name__ == "__main__":
-    main()
+    ## Preparar os dados para serem inseridos no banco de dados
+    ## CLIENTES - Nome, email, telefone, local
